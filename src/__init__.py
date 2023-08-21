068816f65d0c10085561b5cc2dd5139fa4fe73fd
ios <66111703+ioscdn@users.noreply.github.com>
2023-08-21 18:31:49 +0530

    implementation of new config module

--- src/__init__.py
+++ src/__init__.py
@@ -5,14 +5,7 @@ from .modules.config import Config
 from .modules.database import ThreadSafePickleDB
 
 config = Config(
-    default_values={
-        "DB_PATH": "rss-data.json",
-        "DEBUG": "False",
-        "ENTRY_ID_TAG": "title",
-        "ENTRY_EXPIRE_HOURS": "72",
-        "ENTRY_COMPARE_METHOD": "last_published_date",
-        "WORKERS": "5",
-    }
+    default=".env.sample"
 )
 
 DEBUG = "--debug" in sys.argv or config.DEBUG.lower() == "true"
